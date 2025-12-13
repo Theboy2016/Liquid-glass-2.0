985a4df0de236232a3143d0dc7da6d0a04f93093
Theboy2016 <temporarily12133@gmail.com>
2025-12-13 12:23:48 +0000

    Add full Capacitor Android project

---
+++ liquid-glass-camera/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.psjdbd.liquidglass',
+  appName: 'LiquidGlassCamera',
+  webDir: 'dist'
+};
+
+export default config;
