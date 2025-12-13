a9dcc494a4b9477feb09bd4ef5ed5635bcdf747c
Theboy2016 <temporarily12133@gmail.com>
2025-12-13 19:25:52 +0530

    Refactor permission requests in App component

--- App.tsx
+++ App.tsx
@@ -1,25 +1,15 @@
 import React, { useEffect } from 'react';
 import LiquidGlassCamera from './components/LiquidGlassCamera';
-
-import { Camera } from '@capacitor/camera';
-import { Filesystem } from '@capacitor/filesystem';
-import { Capacitor } from '@capacitor/core';
+import { requestAllPermissions } from './permissions';
 
 export default function App() {
 
   useEffect(() => {
-    async function requestPermissions() {
-      if (!Capacitor.isNativePlatform()) return;
-
-      await Camera.requestPermissions();
-      await Filesystem.requestPermissions();
-    }
-
-    requestPermissions();
+    requestAllPermissions();
   }, []);
 
   return (
-    <div className="w-full h-[100vh] bg-black text-white relative overflow-hidden">
+    <div className="w-full h-[100dvh] bg-black text-white overflow-hidden">
       <LiquidGlassCamera />
     </div>
   );
