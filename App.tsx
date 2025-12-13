4696a0e02bafaf8b3be67ec48ed3bb5a34154254
Theboy2016 <temporarily12133@gmail.com>
2025-12-13 23:16:46 +0530

    Update import path for requestAllPermissions

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
 import LiquidGlassCamera from './components/LiquidGlassCamera';
-import { requestAllPermissions } from './permissions';
+import { requestAllPermissions } from './src/utils/permissions';
 
 export default function App() {
-
   useEffect(() => {
     requestAllPermissions();
   }, []);
 
   return (
-    <div className="w-full h-[100dvh] bg-black text-white overflow-hidden">
+    <div className="w-full h-[100dvh] bg-black text-white relative overflow-hidden">
       <LiquidGlassCamera />
     </div>
   );
