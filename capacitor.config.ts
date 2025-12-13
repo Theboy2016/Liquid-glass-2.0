01619deb215c016db6f99947550013bf6479ee0b
Theboy2016 <temporarily12133@gmail.com>
2025-12-13 18:57:14 +0530

    Update Capacitor config for Android settings
    Set bundledWebRuntime to false and allowMixedContent for Android.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.psjdbd.liquidglass',
   appName: 'LiquidGlassCamera',
-  webDir: 'dist'
+  webDir: 'dist',
+  bundledWebRuntime: false,
+
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
