d586532b7cda9db79bd9d2f27c21dc3562b589af
Theboy2016 <temporarily12133@gmail.com>
2025-12-13 23:49:34 +0530

    Fix div tag structure in App.tsx

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   }, []);
 
   return (
-    <div className="w-full h-[100dvh] bg-black text-white relative overflow-hidden">
+    <div className="w-full h-[100dvh] bg-black text-white">
       <LiquidGlassCamera />
     </div>
   );
