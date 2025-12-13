fb19bfa5cb25b87183c108a706be320682693041
Theboy2016 <temporarily12133@gmail.com>
2025-12-13 23:54:02 +0530

    Refactor App component to remove wrapper div
    Simplified the App component by removing unnecessary div wrapper.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react';
-import LiquidGlassCamera from './components/LiquidGlassCamera';
+import { useEffect } from 'react';
 import { requestAllPermissions } from './src/utils/permissions';
+import LiquidGlassCamera from './components/LiquidGlassCamera';
 
 export default function App() {
   useEffect(() => {
     requestAllPermissions();
   }, []);
 
-  return (
-    <div className="w-full h-[100dvh] bg-black text-white">
-      <LiquidGlassCamera />
-    </div>
-  );
+  return <LiquidGlassCamera />;
 }
